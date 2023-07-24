878bd27878f177a75cb767e3f193f76ad58afd2f
Can Arda <canarda.dev@gmail.com>
2023-07-24 17:12:48 +0200

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,51 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+/*
 const x = 23;
 
 const calcAge = (birthYear) => 2037 - birthYear;
-console.log(calcAge(1991));
+console.log(calcAge(1991)); 
+
+[];
+*/
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTemAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTemAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTemAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTemAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
